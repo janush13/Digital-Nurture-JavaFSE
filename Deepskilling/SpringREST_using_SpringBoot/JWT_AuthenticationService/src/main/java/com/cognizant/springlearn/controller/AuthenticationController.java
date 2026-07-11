11ab83b384b8e0c3cf59a5664b825b991d4a6247
Janush D <166034412+janush13@users.noreply.github.com>
2026-07-11 14:04:37 +0530

    Add files via upload

---
+++ Deepskilling/SpringREST_using_SpringBoot/JWT_AuthenticationService/src/main/java/com/cognizant/springlearn/controller/AuthenticationController.java
@@ -0,0 +1,15 @@
+package com.cognizant.springlearn.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthenticationController {
+
+    @GetMapping("/authenticate")
+    public String authenticate() {
+
+        return "Authentication Successful";
+
+    }
+}
